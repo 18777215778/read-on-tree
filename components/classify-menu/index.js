2ba6f24e5e1c5ef246726e05008da36c6a075d5f
18777215778 <18777215778@163.com>
2018-10-25 20:20:19 +0800

    完成首页

---
+++ components/classify-menu/index.js
@@ -0,0 +1,24 @@
+// components/classify-menu/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:String,
+    menu: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/good-books/good-books.js
@@ -0,0 +1,69 @@
+// pages/good-books/good-books.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menu: {
+      '题材': ['科幻', '爱情', '悬疑', '侦探', '历险', '社会'],
+      '难度': ['高中', '四级', '六级', '考研']
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
